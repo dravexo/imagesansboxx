295505bdf36dfd3564faa3db8e8dd5c46e6a6aae
dravexo <nishantkumar9375@gmail.com>
2026-07-10 09:10:54 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './index,html';
+import App from './index.html';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
